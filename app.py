da9d47039f97aaeaec2156088445eb8a3ae97c0a
Khushi2004Shrivastava <142683834+Khushi2004Shrivastava@users.noreply.github.com>
2024-06-13 08:52:50 +0000

    corrections

--- app.py
+++ app.py
@@ -2,30 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-ITEMS = [
-  {
+ITEMS = [{
     'id': 1,
-    'title':'Chemistry Lab Coat',
+    'title': 'Chemistry Lab Coat',
     'Availability': '5',
-    'price':'500'},
-  {
+    'price': '500'
+}, {
     'id': 2,
-    'title':'Engineering Drawing instruments',
+    'title': 'Engineering Drawing instruments',
     'Availability': '4',
-    'price':'free'},
-{
-  'id':3,
-  'title':'handwritten notes 1st sem',
-  'price':'1000'}
-]
+    'price': 'free'
+}, {
+    'id': 3,
+    'title': 'handwritten notes 1st sem',
+    'price': '1000'
+}]
+
 
 @app.route("/")
 def hello_world():
   return render_template('home.html', items=ITEMS, name='BEE')
 
+
 @app.route("/api/items")
 def list_items():
   return jsonify(ITEMS)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
